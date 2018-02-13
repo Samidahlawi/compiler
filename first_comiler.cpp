f6b7f455111edcb83afde5308683a95baab539f9
wh0-1-am <o.t5@hotmail.com>
2018-02-13 21:32:22 +0300

    first comment

---
+++ first_comiler.cpp
@@ -0,0 +1,17 @@
+//This file is comiler for english statment.
+/*The idea of program is read the statment from user then
+show the resulit of how many word, letter and number.
+It is so easy program (:
+*/
+*/OUR TEAM
+ - wayfarer
+ -suliman
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "hello compiler again " << endl;
+return 0;
+}
